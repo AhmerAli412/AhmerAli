e1029166fabadb598a8bd68f57f44759d6e3b813
Ahmer Ali <aliahmer289@gmail.com>
2023-12-30 21:38:36 +0500

    git cont added

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,3 +219,70 @@ var typed = new Typed(".type", {
   backSpeed: 60,
   loop: true,
 });
+
+
+const username = "AhmerAli412"; // Replace with your Github username
+const accessToken = "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"; // Replace with your personal access token
+const apiUrl = "https://api.github.com/graphql";
+
+const query = `
+  query {
+    user(login: "${username}") {
+      contributionsCollection {
+        contributionCalendar {
+          totalContributions
+          weeks {
+            contributionDays {
+              contributionCount
+              date
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+fetch(apiUrl, {
+  method: "POST",
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ query }),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    const contributions = data.data.user.contributionsCollection.contributionCalendar;
+    const weeks = contributions.weeks;
+
+    // Create the table structure based on contributions data
+    const contributionTable = document.getElementById("contribution-table");
+    for (let dayOfWeek = 0; dayOfWeek < 7; dayOfWeek++) {
+      const row = document.createElement("tr");
+      for (let weekIndex = 0; weekIndex < 53; weekIndex++) {
+        const week = weeks[weekIndex];
+        const contributionDay = week.contributionDays[dayOfWeek];
+        const contributionCount = contributionDay ? contributionDay.contributionCount : 0;
+        const td = document.createElement("td");
+        let contributionColor;
+        if (contributionCount < 1) {
+          contributionColor = 0;
+        } else if (contributionCount > 1) {
+          contributionColor = 1;
+        } else if (contributionCount > 5) {
+          contributionColor = 2;
+        } else if (contributionCount < 20) {
+          contributionColor = 3;
+        } else {
+          contributionColor = 4;
+        }
+        td.className = `color-${contributionColor}`;
+        row.appendChild(td);
+      }
+      contributionTable.appendChild(row);
+    }
+  })
+  .catch((error) => {
+    console.error("Error fetching GitHub contributions:", error);
+  });
